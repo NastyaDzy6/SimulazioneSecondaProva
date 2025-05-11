1ef4e1081c6be94bfc54401b169c15f3bbfff593
LAPTOP-RTRKG73N\39329 <dzyuvanovvanastasia@gmail.com>
2025-05-11 14:17:10 +0200

    cambiato db

--- terminalnavi.sql
+++ terminalnavi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mag 05, 2025 alle 21:14
+-- Creato il: Mag 11, 2025 alle 14:16
 -- Versione del server: 10.4.32-MariaDB
 -- Versione PHP: 8.2.12
 
@@ -24,17 +24,17 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `buono_consegna`
+-- Struttura della tabella `buonoconsegna`
 --
 
-CREATE TABLE `buono_consegna` (
-  `id_buono` int(11) NOT NULL,
-  `id_merce` int(11) NOT NULL,
-  `id_camionista` int(11) NOT NULL,
-  `targa` varchar(20) NOT NULL,
-  `telefono_cliente` varchar(20) DEFAULT NULL,
-  `data_ritiro` date DEFAULT NULL,
-  `peso_ritirato` decimal(10,2) DEFAULT NULL
+CREATE TABLE `buonoconsegna` (
+  `idBuono` int(11) NOT NULL,
+  `merce_id` int(11) NOT NULL,
+  `camionista_id` int(11) NOT NULL,
+  `camion_id` varchar(20) NOT NULL,
+  `telefonoCliente` varchar(20) DEFAULT NULL,
+  `dataRitiro` date DEFAULT NULL,
+  `pesoRitirato` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -44,6 +44,7 @@ CREATE TABLE `buono_consegna` (
 --
 
 CREATE TABLE `camion` (
+  `marca` varchar(50) DEFAULT NULL,
   `targa` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -54,10 +55,11 @@ CREATE TABLE `camion` (
 --
 
 CREATE TABLE `camionista` (
-  `id_camionista` int(11) NOT NULL,
-  `nome` varchar(100) DEFAULT NULL,
-  `cognome` varchar(100) DEFAULT NULL,
-  `telefono` varchar(20) DEFAULT NULL
+  `idCamionista` int(11) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `cognome` varchar(50) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `targaCamion` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -67,23 +69,37 @@ CREATE TABLE `camionista` (
 --
 
 CREATE TABLE `cliente` (
-  `id_cliente` int(11) NOT NULL,
-  `nome` varchar(100) DEFAULT NULL,
-  `cognome` varchar(100) DEFAULT NULL,
+  `idCliente` int(11) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `cognome` varchar(50) DEFAULT NULL,
   `telefono` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `guida_camion`
+-- Struttura della tabella `fornitore`
 --
 
-CREATE TABLE `guida_camion` (
-  `id_camionista` int(11) NOT NULL,
+CREATE TABLE `fornitore` (
+  `idFornitore` int(11) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `cognome` varchar(50) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `nomeAzienda` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `guidacamion`
+--
+
+CREATE TABLE `guidacamion` (
+  `idCamionista` int(11) NOT NULL,
   `targa` varchar(20) NOT NULL,
-  `data_inizio` date NOT NULL,
-  `data_fine` date DEFAULT NULL
+  `dataInizio` date NOT NULL,
+  `dataFine` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -93,12 +109,13 @@ CREATE TABLE `guida_camion` (
 --
 
 CREATE TABLE `merce` (
-  `id_merce` int(11) NOT NULL,
+  `idMerce` int(11) NOT NULL,
   `tipologia` varchar(100) DEFAULT NULL,
-  `peso` decimal(5,2) DEFAULT NULL,
-  `volume` decimal(5,2) DEFAULT NULL,
-  `condizioni_conservazione` varchar(255) DEFAULT NULL,
-  `id_polizza` int(11) DEFAULT NULL
+  `peso` double DEFAULT NULL,
+  `volume` double DEFAULT NULL,
+  `condizioniConservazione` varchar(100) DEFAULT NULL,
+  `polizza_id` int(11) DEFAULT NULL,
+  `idFornitore` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -108,8 +125,8 @@ CREATE TABLE `merce` (
 --
 
 CREATE TABLE `nave` (
-  `id_nave` int(11) NOT NULL,
-  `nome` varchar(100) NOT NULL
+  `idNave` int(11) NOT NULL,
+  `nome` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -119,12 +136,12 @@ CREATE TABLE `nave` (
 --
 
 CREATE TABLE `polizza` (
-  `id_polizza` int(11) NOT NULL,
-  `franchigia_giorni` int(11) DEFAULT NULL,
+  `idPolizza` int(11) NOT NULL,
+  `franchigiaGiorni` int(11) DEFAULT NULL,
   `fornitore` varchar(100) DEFAULT NULL,
   `destinazione` varchar(100) DEFAULT NULL,
-  `porto_carico` int(11) DEFAULT NULL,
-  `id_viaggio` int(11) DEFAULT NULL
+  `portoCarico_id` int(11) DEFAULT NULL,
+  `viaggio_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -134,10 +151,22 @@ CREATE TABLE `polizza` (
 --
 
 CREATE TABLE `porto` (
-  `id_porto` int(11) NOT NULL,
-  `nome` varchar(100) NOT NULL,
-  `nazione` varchar(100) NOT NULL,
+  `idPorto` int(11) NOT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `nazione` varchar(100) DEFAULT NULL,
   `linea` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `utente`
+--
+
+CREATE TABLE `utente` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `ruolo` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -147,12 +176,12 @@ CREATE TABLE `porto` (
 --
 
 CREATE TABLE `viaggio` (
-  `id_viaggio` int(11) NOT NULL,
-  `id_nave` int(11) NOT NULL,
-  `data_partenza` date NOT NULL,
-  `data_arribamento` date DEFAULT NULL,
-  `porto_partenza` int(11) NOT NULL,
-  `porto_arrivo` int(11) NOT NULL
+  `idViaggio` int(11) NOT NULL,
+  `idNave` int(11) DEFAULT NULL,
+  `dataPartenza` date DEFAULT NULL,
+  `dataArribamento` date DEFAULT NULL,
+  `portoPartenza_id` int(11) DEFAULT NULL,
+  `portoArrivo_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -160,13 +189,13 @@ CREATE TABLE `viaggio` (
 --
 
 --
--- Indici per le tabelle `buono_consegna`
+-- Indici per le tabelle `buonoconsegna`
 --
-ALTER TABLE `buono_consegna`
-  ADD PRIMARY KEY (`id_buono`),
-  ADD KEY `id_merce` (`id_merce`),
-  ADD KEY `id_camionista` (`id_camionista`),
-  ADD KEY `targa` (`targa`);
+ALTER TABLE `buonoconsegna`
+  ADD PRIMARY KEY (`idBuono`),
+  ADD KEY `merce_id` (`merce_id`),
+  ADD KEY `camionista_id` (`camionista_id`),
+  ADD KEY `camion_id` (`camion_id`);
 
 --
 -- Indici per le tabelle `camion`
@@ -178,106 +207,117 @@ ALTER TABLE `camion`
 -- Indici per le tabelle `camionista`
 --
 ALTER TABLE `camionista`
-  ADD PRIMARY KEY (`id_camionista`);
+  ADD PRIMARY KEY (`idCamionista`),
+  ADD KEY `targaCamion` (`targaCamion`);
 
 --
 -- Indici per le tabelle `cliente`
 --
 ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`id_cliente`);
+  ADD PRIMARY KEY (`idCliente`);
 
 --
--- Indici per le tabelle `guida_camion`
+-- Indici per le tabelle `fornitore`
 --
-ALTER TABLE `guida_camion`
-  ADD PRIMARY KEY (`id_camionista`,`targa`,`data_inizio`),
+ALTER TABLE `fornitore`
+  ADD PRIMARY KEY (`idFornitore`);
+
+--
+-- Indici per le tabelle `guidacamion`
+--
+ALTER TABLE `guidacamion`
+  ADD PRIMARY KEY (`idCamionista`,`targa`,`dataInizio`),
   ADD KEY `targa` (`targa`);
 
 --
 -- Indici per le tabelle `merce`
 --
 ALTER TABLE `merce`
-  ADD PRIMARY KEY (`id_merce`),
-  ADD KEY `id_polizza` (`id_polizza`);
+  ADD PRIMARY KEY (`idMerce`),
+  ADD KEY `polizza_id` (`polizza_id`),
+  ADD KEY `idFornitore` (`idFornitore`);
 
 --
 -- Indici per le tabelle `nave`
 --
 ALTER TABLE `nave`
-  ADD PRIMARY KEY (`id_nave`);
+  ADD PRIMARY KEY (`idNave`);
 
 --
 -- Indici per le tabelle `polizza`
 --
 ALTER TABLE `polizza`
-  ADD PRIMARY KEY (`id_polizza`),
-  ADD KEY `porto_carico` (`porto_carico`),
-  ADD KEY `id_viaggio` (`id_viaggio`);
+  ADD PRIMARY KEY (`idPolizza`),
+  ADD KEY `portoCarico_id` (`portoCarico_id`),
+  ADD KEY `viaggio_id` (`viaggio_id`);
 
 --
 -- Indici per le tabelle `porto`
 --
 ALTER TABLE `porto`
-  ADD PRIMARY KEY (`id_porto`);
+  ADD PRIMARY KEY (`idPorto`);
+
+--
+-- Indici per le tabelle `utente`
+--
+ALTER TABLE `utente`
+  ADD PRIMARY KEY (`username`);
 
 --
 -- Indici per le tabelle `viaggio`
 --
 ALTER TABLE `viaggio`
-  ADD PRIMARY KEY (`id_viaggio`),
-  ADD KEY `id_nave` (`id_nave`),
-  ADD KEY `porto_partenza` (`porto_partenza`),
-  ADD KEY `porto_arrivo` (`porto_arrivo`);
-
---
--- AUTO_INCREMENT per le tabelle scaricate
---
-
---
--- AUTO_INCREMENT per la tabella `cliente`
---
-ALTER TABLE `cliente`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
+  ADD PRIMARY KEY (`idViaggio`),
+  ADD KEY `idNave` (`idNave`),
+  ADD KEY `portoPartenza_id` (`portoPartenza_id`),
+  ADD KEY `portoArrivo_id` (`portoArrivo_id`);
 
 --
 -- Limiti per le tabelle scaricate
 --
 
 --
--- Limiti per la tabella `buono_consegna`
+-- Limiti per la tabella `buonoconsegna`
 --
-ALTER TABLE `buono_consegna`
-  ADD CONSTRAINT `buono_consegna_ibfk_1` FOREIGN KEY (`id_merce`) REFERENCES `merce` (`id_merce`),
-  ADD CONSTRAINT `buono_consegna_ibfk_2` FOREIGN KEY (`id_camionista`) REFERENCES `camionista` (`id_camionista`),
-  ADD CONSTRAINT `buono_consegna_ibfk_3` FOREIGN KEY (`targa`) REFERENCES `camion` (`targa`);
+ALTER TABLE `buonoconsegna`
+  ADD CONSTRAINT `buonoconsegna_ibfk_1` FOREIGN KEY (`merce_id`) REFERENCES `merce` (`idMerce`),
+  ADD CONSTRAINT `buonoconsegna_ibfk_2` FOREIGN KEY (`camionista_id`) REFERENCES `camionista` (`idCamionista`),
+  ADD CONSTRAINT `buonoconsegna_ibfk_3` FOREIGN KEY (`camion_id`) REFERENCES `camion` (`targa`);
 
 --
--- Limiti per la tabella `guida_camion`
+-- Limiti per la tabella `camionista`
 --
-ALTER TABLE `guida_camion`
-  ADD CONSTRAINT `guida_camion_ibfk_1` FOREIGN KEY (`id_camionista`) REFERENCES `camionista` (`id_camionista`),
-  ADD CONSTRAINT `guida_camion_ibfk_2` FOREIGN KEY (`targa`) REFERENCES `camion` (`targa`);
+ALTER TABLE `camionista`
+  ADD CONSTRAINT `camionista_ibfk_1` FOREIGN KEY (`targaCamion`) REFERENCES `camion` (`targa`);
+
+--
+-- Limiti per la tabella `guidacamion`
+--
+ALTER TABLE `guidacamion`
+  ADD CONSTRAINT `guidacamion_ibfk_1` FOREIGN KEY (`idCamionista`) REFERENCES `camionista` (`idCamionista`),
+  ADD CONSTRAINT `guidacamion_ibfk_2` FOREIGN KEY (`targa`) REFERENCES `camion` (`targa`);
 
 --
 -- Limiti per la tabella `merce`
 --
 ALTER TABLE `merce`
-  ADD CONSTRAINT `merce_ibfk_1` FOREIGN KEY (`id_polizza`) REFERENCES `polizza` (`id_polizza`);
+  ADD CONSTRAINT `merce_ibfk_1` FOREIGN KEY (`polizza_id`) REFERENCES `polizza` (`idPolizza`),
+  ADD CONSTRAINT `merce_ibfk_2` FOREIGN KEY (`idFornitore`) REFERENCES `fornitore` (`idFornitore`);
 
 --
 -- Limiti per la tabella `polizza`
 --
 ALTER TABLE `polizza`
-  ADD CONSTRAINT `polizza_ibfk_1` FOREIGN KEY (`porto_carico`) REFERENCES `porto` (`id_porto`),
-  ADD CONSTRAINT `polizza_ibfk_2` FOREIGN KEY (`id_viaggio`) REFERENCES `viaggio` (`id_viaggio`);
+  ADD CONSTRAINT `polizza_ibfk_1` FOREIGN KEY (`portoCarico_id`) REFERENCES `porto` (`idPorto`),
+  ADD CONSTRAINT `polizza_ibfk_2` FOREIGN KEY (`viaggio_id`) REFERENCES `viaggio` (`idViaggio`);
 
 --
 -- Limiti per la tabella `viaggio`
 --
 ALTER TABLE `viaggio`
-  ADD CONSTRAINT `viaggio_ibfk_1` FOREIGN KEY (`id_nave`) REFERENCES `nave` (`id_nave`),
-  ADD CONSTRAINT `viaggio_ibfk_2` FOREIGN KEY (`porto_partenza`) REFERENCES `porto` (`id_porto`),
-  ADD CONSTRAINT `viaggio_ibfk_3` FOREIGN KEY (`porto_arrivo`) REFERENCES `porto` (`id_porto`);
+  ADD CONSTRAINT `viaggio_ibfk_1` FOREIGN KEY (`idNave`) REFERENCES `nave` (`idNave`),
+  ADD CONSTRAINT `viaggio_ibfk_2` FOREIGN KEY (`portoPartenza_id`) REFERENCES `porto` (`idPorto`),
+  ADD CONSTRAINT `viaggio_ibfk_3` FOREIGN KEY (`portoArrivo_id`) REFERENCES `porto` (`idPorto`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
