6befa0f8d522292771d4261cd32adb3a3cca3abf
LAPTOP-RTRKG73N\39329 <dzyuvanovvanastasia@gmail.com>
2025-05-13 20:57:02 +0200

    riffato spring perche ho eliminato una delle cartelle per sbaglio

---
+++ terminalbackend/src/main/java/com/terminal/terminalbackend/TerminalbackendApplication.java
@@ -0,0 +1,13 @@
+package com.terminal.terminalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminalbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerminalbackendApplication.class, args);
+	}
+
+}
--- terminalbackend/src/test/java/com/terminal/terminalbackend/TerminalbackendApplicationTests.java
+++ terminalbackend/src/test/java/com/terminal/terminalbackend/TerminalbackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.terminaldzy.terminal;
+package com.terminal.terminalbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TerminalApplicationTests {
+class TerminalbackendApplicationTests {
 
 	@Test
 	void contextLoads() {
